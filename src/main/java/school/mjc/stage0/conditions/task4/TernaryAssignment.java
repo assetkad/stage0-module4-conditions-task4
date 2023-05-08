857b28e63455cb4afe56b94d4948ef61bef735cc
Asset Kad <asset_kad@mail.ru>
2023-05-08 14:45:42 +0500

    finished stage 0, module #4.1, task#4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -1,6 +1,8 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
-    public void assignAndPrintBasedOnWhichBigger(int first, int second) {
-    }
+  public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+    var result = first > second ? "10" : "-10";
+    System.out.println(result);
+  }
 }
